0f60ca03e24e16a806e08a9a4aac9bd746ea8c4a
Burak Aydın <burakaydin99@gmail.com>
2023-01-31 16:19:28 +0300

    ilk commit

---
+++ client/src/pages/Admin/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Box ,Image } from "@chakra-ui/react";
+
+
+function Home() {
+  return (
+    <div>
+<Box boxSize='m'>
+  <Image src='https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce.jpg' alt='E-Commerce' borderRadius="full"/>
+</Box>
+    </div>
+  )
+}
+
+export default Home
